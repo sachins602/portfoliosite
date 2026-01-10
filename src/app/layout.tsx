f5b7284f0af5293a6ee4b6edd6459ce92b91e183
sachin <sachinspkota4@gmail.com>
2026-01-09 20:27:59 -0500

    init

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import "~/styles/globals.css";
+
+import type { Metadata } from "next";
+import { Geist } from "next/font/google";
+
+import { TRPCReactProvider } from "~/trpc/react";
+
+export const metadata: Metadata = {
+	title: "Sachin Sapkota",
+	description: "Sachin's Portfolio",
+	icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+const geist = Geist({
+	subsets: ["latin"],
+	variable: "--font-geist-sans",
+});
+
+export default function RootLayout({
+	children,
+}: Readonly<{ children: React.ReactNode }>) {
+	return (
+		<html className={`${geist.variable}`} lang="en">
+			<body>
+				<TRPCReactProvider>{children}</TRPCReactProvider>
+			</body>
+		</html>
+	);
+}
